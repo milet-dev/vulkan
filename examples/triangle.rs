444916838e67aac00b2c8afd9c6229d4e28a352e
milet <milet@milet.org>
2023-05-30 13:19:31 +0300

    Start implementing triangle example

---
+++ examples/triangle.rs
@@ -0,0 +1,67 @@
+use vulkan::prelude::*;
+
+use winit::{
+    event::{Event, KeyboardInput, VirtualKeyCode, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::{WindowBuilder, WindowButtons},
+};
+
+const TITLE: &str = "vulkan";
+
+fn initialize_vulkan(handle: &impl raw_window_handle::HasRawWindowHandle) -> anyhow::Result<()> {
+    let instance = Instance::new()?;
+    let surface = Surface::new(handle, &instance)?;
+    let physical_device = instance
+        .physical_devices()?
+        .into_iter()
+        .find(|physical_device| {
+            physical_device
+                .properties
+                .device_type
+                .eq(&vk::PhysicalDeviceType::DISCRETE_GPU)
+        })
+        .unwrap();
+    let device = Device::new(&instance, &physical_device)?;
+    let swapchain = Swapchain::new(&instance, &surface, &physical_device, &device)?;
+
+    Ok(())
+}
+
+fn main() -> anyhow::Result<()> {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title(TITLE)
+        .with_resizable(false)
+        .with_enabled_buttons(WindowButtons::MINIMIZE | WindowButtons::CLOSE)
+        .build(&event_loop)?;
+
+    initialize_vulkan(&window)?;
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            }
+            | Event::WindowEvent {
+                window_id,
+                event:
+                    WindowEvent::KeyboardInput {
+                        input:
+                            KeyboardInput {
+                                virtual_keycode: Some(VirtualKeyCode::Escape),
+                                ..
+                            },
+                        ..
+                    },
+            } => {
+                if window_id == window.id() {
+                    *control_flow = ControlFlow::Exit;
+                }
+            }
+            _ => (),
+        }
+    });
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,15 @@ pub mod instance;
 pub mod physical_device;
 pub mod surface;
 pub mod swapchain;
+
+pub mod prelude {
+    pub use ash::vk;
+
+    pub use crate::{
+        device::Device,
+        instance::Instance,
+        physical_device::{PhysicalDevice, QueueFamily},
+        surface::Surface,
+        swapchain::Swapchain,
+    };
+}

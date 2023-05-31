a8089e553feb4c1b06de70369dd18d5ba510754b
milet <milet@milet.org>
2023-05-31 21:55:44 +0300

    Enable dynamic rendering

--- src/device.rs
+++ src/device.rs
@@ -1,12 +1,16 @@
 use std::slice;
 
-use ash::{extensions::khr, vk};
+use ash::{
+    extensions::khr::{self, DynamicRendering},
+    vk,
+};
 
 use crate::{instance::Instance, physical_device::PhysicalDevice};
 
 pub struct Device {
     pub handle: ash::Device,
     pub queue: vk::Queue,
+    pub dynamic_rendering: khr::DynamicRendering,
 }
 
 impl Device {
@@ -28,19 +32,39 @@ impl Device {
             .queue_priorities(&[1.0]);
 
         let swapchain_ext = khr::Swapchain::name();
+        let dynamic_rendering_ext = ash::extensions::khr::DynamicRendering::name();
+        let enabled_extensions = [swapchain_ext.as_ptr(), dynamic_rendering_ext.as_ptr()];
 
         let handle = unsafe {
+            let mut dynamic_rendering =
+                vk::PhysicalDeviceDynamicRenderingFeatures::builder().dynamic_rendering(true);
+            let mut features = vk::PhysicalDeviceFeatures2::builder()
+                .push_next(&mut dynamic_rendering)
+                .features(
+                    vk::PhysicalDeviceFeatures::builder()
+                        .sampler_anisotropy(true)
+                        .fill_mode_non_solid(true)
+                        .sample_rate_shading(true)
+                        .build(),
+                );
             instance.handle.create_device(
                 physical_device.handle,
                 &vk::DeviceCreateInfo::builder()
+                    .push_next(&mut features)
                     .queue_create_infos(slice::from_ref(&queue_create_info))
-                    .enabled_extension_names(slice::from_ref(&swapchain_ext.as_ptr())),
+                    .enabled_extension_names(&enabled_extensions),
                 None,
             )?
         };
 
         let queue = unsafe { handle.get_device_queue(queue_family.index, 0) };
 
-        Ok(Device { handle, queue })
+        let dynamic_rendering = DynamicRendering::new(&instance.handle, &handle);
+
+        Ok(Device {
+            handle,
+            queue,
+            dynamic_rendering,
+        })
     }
 }
